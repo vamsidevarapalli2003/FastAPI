f723eb4c15cc15feef8cba16998dce413b9103cb
vamsi <vamsidevarapalli2003@gmail.com>
2026-08-25 11:24:36 +0530

    postgrsssql

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://deploy_database_mbi7_user:UwRyH78pCa1GP5gZuASrwItGa2wIpkPU@dpg-da6iotn10e5c73bs816g-a.singapore-postgres.render.com/deploy_database_mbi7'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
